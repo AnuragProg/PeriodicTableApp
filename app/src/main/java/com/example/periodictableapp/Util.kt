8332a723a5ba2634881caa20089bffa7cd221b17
Anurag Singh <anuragcollege7@gmail.com>
2022-02-15 19:06:32 +0545

    Adding utility class to project

---
+++ app/src/main/java/com/example/periodictableapp/Util.kt
@@ -0,0 +1,20 @@
+package com.example.periodictableapp
+
+import android.content.Context
+import java.io.IOException
+
+
+private val FILENAME = "periodic_table.json"
+fun getPeriodicTableFromAsset(context: Context, filename:String=FILENAME) : String?{
+    val json : String
+    return try{
+        json = context.assets.open(filename).bufferedReader().use{ it.readText()}
+        json
+
+    }catch (ioException : IOException){
+        ioException.printStackTrace()
+        null
+    }
+}
+
+
